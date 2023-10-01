876ab1c582909bda22260ee7411e9006994068e6
Haider Khalil <haider.khalil@nkutechnologies.com>
2023-10-01 15:27:34 +0500

    Add project files.

---
+++ FirstApp/Controllers/FirstApp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controllers
+{
+    public class FirstApp : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string App()
+        //{
+        //    return "HEllo World";
+        //}
+
+
+    }
+}
